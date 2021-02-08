ba8c41617a93c29f3465fa604f3f619e61c23190
emelie <emeliejlindvall@gmail.com>
2021-02-08 20:46:43 +0100

    Changed layout in FilterView

--- HikePackr/FilterView.swift
+++ HikePackr/FilterView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct FilterView: View {
+    
+    // FAKE FOR LAYOUT
+    var isChecked = false
+    
+    // FAKE FOR LAYOUT
+    @State var numberOfDays = 1
+   
     var body: some View {
-        Text("Filter View")
+        VStack {
+            Form {
+                Section(header: Text("Degrees")) {
+                    Text("Slider goes here")
+                }
+                Section(header: Text("Type of stay")) {
+                    HStack {
+                        Image(systemName: isChecked ? "xmark.square" : "square")
+                        Text("Tent")
+                    }
+                    HStack {
+                        Image(systemName: isChecked ? "xmark.square" : "square")
+                        Text("Cabin")
+                    }
+                    HStack {
+                        Image(systemName: isChecked ? "xmark.square" : "square")
+                        Text("Hotel")
+                    }
+                }
+                Section(header: Text("Number of days")) {
+                    Stepper("\(numberOfDays) day(s)", value: $numberOfDays, in: 1...10)
+                }
+                Button(action: {
+                    print("Apply")
+                }, label: {
+                    Text("Apply")
+                })
+            }
+            
+        }
     }
 }
 
